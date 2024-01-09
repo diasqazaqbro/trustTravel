5ce878b56f9767f0a88e3d687f021dbfb984ba8b
Nruzhzhaugash <clonn262@gmail.com>
2024-01-09 07:16:31 +0600

    mobile addaptive tour

--- src/shared/ui/Rating/Rating.tsx
+++ src/shared/ui/Rating/Rating.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
 
 export default function BasicRating() {
   const [value, setValue] = useState<number | null>(2);
